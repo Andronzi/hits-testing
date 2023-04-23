8fe5a947910631c32d99ac8aa0a1f5bb2ad911b6
Andronzi <andrey.radioniv.2003@gmail.com>
2023-04-24 00:10:37 +0700

    :bug: fix(custom): Throw error if h less then piles length

--- src/koko.ts
+++ src/koko.ts
@@ -26,18 +26,22 @@ Output: 23
 */
 
 const minEatingSpeed = function (piles: number[], h: number) {
-    let [left, right] = [1, Math.max(...piles)];
-  
-    while (left < right) {
-      const mid = (left + right) >> 1;
-      const hourSpent = getHourSpent(mid, piles);
-  
-      const isTargetGreater = h < hourSpent;
-      if (isTargetGreater) left = mid + 1;
-  
-      const isTargetLess = hourSpent <= h;
-      if (isTargetLess) right = mid;
-    }
+  if (h < piles.length) {
+    throw new Error();
+  }
+
+  let [left, right] = [1, Math.max(...piles)];
+
+  while (left < right) {
+    const mid = (left + right) >> 1;
+    const hourSpent = getHourSpent(mid, piles);
+
+    const isTargetGreater = h < hourSpent;
+    if (isTargetGreater) left = mid + 1;
+
+    const isTargetLess = hourSpent <= h;
+    if (isTargetLess) right = mid;
+  }
   
     return right;
   };
