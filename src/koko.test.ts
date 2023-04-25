526fd1b992d740dffa36e4129c74c56afaf986ec
Andronzi <andrey.radioniv.2003@gmail.com>
2023-04-25 11:18:34 +0700

    :white_check_mark: test: Add parameterized tests to check piles boundaries

--- src/koko.test.ts
+++ src/koko.test.ts
@@ -20,9 +20,9 @@ describe("basic tests", () => {
 /*
 CONSTRAINTS
 
-1 <= piles.length <= 104
-piles.length <= h <= 109
-1 <= piles[i] <= 109
+1 <= piles.length <= 10^4
+piles.length <= h <= 10^9
+1 <= piles[i] <= 10^9
 
 */
 
@@ -30,4 +30,16 @@ describe("h less than piles array length", () => {
     test.each([[basicPilesArray.length - 1], [1], [0], [-1], [-10000]])("should return proper result when passed arguments are: %i, %i", (h) => {
         expect(() => minEatingSpeed(basicPilesArray, h)).toThrow();
     })
+});
+
+describe("check constrains for piles", () => {
+	test.each([
+		[[], 1], 
+		[new Array(100).fill(0), 10], 
+		[new Array(100).fill(1_000_000_001), 10], 
+		[new Array(100).fill(10), 1_000_000_001]
+	])
+	("should return exception when passed arguments are: %i, %i", (piles, h) => {
+		expect(() => minEatingSpeed(piles, h)).toThrow();
+	})
 });
