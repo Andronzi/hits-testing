8258b844f7e328f2dff914039b5bdb200cdf06f6
Andronzi <andrey.radioniv.2003@gmail.com>
2023-04-25 10:41:35 +0700

    :white_check_mark: test(custom): Add parametrizing tests to check piles length constraints

--- src/koko.test.ts
+++ src/koko.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, test } from "@jest/globals";
 import minEatingSpeed from "./koko.js";
 
+const basicPilesArray = [30, 11, 23, 4, 20];
+
 describe("basic tests", () => {
     test.each([
         [[3, 6, 7, 11], 8, 4],
@@ -13,4 +15,19 @@ describe("basic tests", () => {
             expect(minEatingSpeed(piles, h)).toBe(result)
         }
     )
+});
+
+/*
+CONSTRAINTS
+
+1 <= piles.length <= 104
+piles.length <= h <= 109
+1 <= piles[i] <= 109
+
+*/
+
+describe("h less than piles array length", () => {
+    test.each([[basicPilesArray.length - 1], [1], [0], [-1], [-10000]])("should return proper result when passed arguments are: %i, %i", (h) => {
+        expect(() => minEatingSpeed(basicPilesArray, h)).toThrow();
+    })
 });
